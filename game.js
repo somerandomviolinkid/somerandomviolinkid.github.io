19640bff758a66ac6acf8f2cc79315bc13f31ec4
somerandomviolinkid <84521318+somerandomviolinkid@users.noreply.github.com>
2022-07-29 16:03:00 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,88 @@
-let resourceAmounts = [money = 0, resources = 0, refinedResources = 0, workers = 0, energy = 0, researchPoints = 0];
+let money = 0;
+let resources = 0;
+let refinedResources = 0;
+let workers = 0;
+let energy = 0;
+let researchPoints = 0;
 
-function tick() {
+let workerLevel = 0;
+let workerEfficiency = [1, 2, 4, 10, 25, 75, 200, 500, 1000]
+let workerEfficiencyCosts = [0, 25, 100, 500, 2500, 10000, 50000, 200000, 1000000]
+
+let resourceSellRate = 5;
+
+function tick1() {
 
     //updates numbers on page
-    document.getElementById("moneyCount").innerHTML = "Money: " + resourceAmounts[money];
-    document.getElementById("workerCount").innerHTML = "Workers: " + resourceAmounts[workers];  
-    document.getElementById("resourceCount").innerHTML = "Resources: " + resourceAmounts[resources];
-    document.getElementById("refinedResourceCount").innerHTML = "Refined resources: " + resourceAmounts[refinedResources];          
-    document.getElementById("energyCount").innerHTML = "Energy: " + resourceAmounts[energy];
-    document.getElementById("researchPointCount").innerHTML = "Research points: " + resourceAmounts[researchPoints];
+    document.getElementById("moneyCount").innerHTML = "Money: " + money;
+    document.getElementById("workerCount").innerHTML = "Workers: " + workers;
+    document.getElementById("resourceCount").innerHTML = "Resources: " + resources;
+    document.getElementById("refinedResourceCount").innerHTML = "Refined resources: " + refinedResources;
+    document.getElementById("energyCount").innerHTML = "Energy: " + energy;
+    document.getElementById("researchPointCount").innerHTML = "Research points: " + researchPoints;
 
 }
 
-tick();
+tick1();
+
+function tick2() {
+
+    resources += workers * workerEfficiency[workerLevel];
+    tick1();
+
+}
+
+setInterval(tick2, 1000);
+
+function manualResources() {
+
+    //gives resources on click
+    resources++;
+    tick1();
+
+}
+
+function sellResources() {
+
+    //sells resources
+    money += resources * resourceSellRate;
+    resources = 0;
+    tick1();
+
+}
+
+function purchaseWorkers() {
+
+    //buys workers
+    if (money > 10) {
+
+        money -= 10;
+        workers++;
+        tick1();
+
+    }
+
+}
+
+document.getElementById("upgradeWorkerButton").innerHTML = "Your workers are level " + workerLevel + ". Upgrade worker cost " + workerEfficiencyCosts[workerLevel + 1];
+
+function upgradeWorkers() {
+
+    //upgrades workers
+    if (money >= workerEfficiencyCosts[workerLevel + 1]) {
+
+        money -= workerEfficiencyCosts[workerLevel + 1];
+        workerLevel++;
+        document.getElementById("upgradeWorkerButton").innerHTML = "Your workers are level " + workerLevel + ". Upgrade worker cost " + workerEfficiencyCosts[workerLevel + 1];
+
+    }
+
+    //hides button once workers are at maximum level
+    if (workerLevel == 8) {
+
+        document.getElementById("upgradeWorkerButton").style.visibility = "hidden";
+
+    }
+
+
+}
