8d1f1c9254577fa1ca160596456a69c7543fe8be
Tobias Berger <tobi.berger13@gmail.com>
2022-08-16 21:13:04 +0200

    Use classList more consistently

--- page.js
+++ page.js
@@ -8,7 +8,7 @@ function openTab(evt, tabName) {
   // Get all elements with class="tablinks" and remove the class "active"
   const tablinks = document.getElementsByClassName("tablinks");
   for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].classList.remove("active");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
